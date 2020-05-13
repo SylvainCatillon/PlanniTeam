a8ad48c5cbb53ae1e70bf9bc816493e250434a08
SylvainCatillon <sylvain.catillon@gmail.com>
2020-05-13 19:32:40 +0200

    Settings module instead of settings file

---
+++ PlanniTeam/settings/travis.py
@@ -0,0 +1,12 @@
+from PlanniTeam.settings import *
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': '',
+    'USER': 'postgres',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': '',
+    },
+}
