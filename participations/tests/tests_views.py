33c06eee514deaa05e4a88d5219c6d4a62363a1a
SylvainCatillon <sylvain.catillon@gmail.com>
2020-05-27 19:16:57 +0200

    App participations: -Planning participations (view django + JQuery) -Test Selenium

--- participations/tests/tests_views.py
+++ participations/tests/tests_views.py
@@ -1,8 +1,11 @@
+from json import dumps
+
 from django.test import TestCase
 from django.urls import reverse
 
 from accounts.models import User
 from plannings.models import Planning, Event
+from participations.models import Participation
 
 
 class DisplayViewTestCase(TestCase):
@@ -68,7 +71,6 @@ class DisplayViewTestCase(TestCase):
         self.assertTemplateNotUsed(response,
                                    'participations/protected_planning.html')
 
-
     def test_participants_in_context(self):
         response = self.client.get(reverse('participations:view', kwargs={
             'planning_ekey': self.key}))
@@ -115,3 +117,97 @@ class DisplayViewTestCase(TestCase):
                         answer=answer):
                     self.assertIn(participation.participant.first_name,
                                   event.participations_summary[answer])
+
+
+class ParticipateViewTestCase(TestCase):
+    fixtures = ['users', 'plannings']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.events = Planning.objects.get(name='Test').event_set
+        cls.user = User.objects.get(first_name='Participant')
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_post_participate_returns_200(self):
+        response = self.client.post(reverse('participations:participate'))
+        self.assertEqual(200, response.status_code)
+
+    def test_get_participate_returns_405(self):
+        response = self.client.get(reverse('participations:participate'))
+        self.assertEqual(405, response.status_code)
+
+    def test_post_un_logged_returns_401(self):
+        self.client.logout()
+        response = self.client.post(reverse('participations:participate'))
+        self.assertEqual(401, response.status_code)
+
+    def test_post_by_stranger_protected_planning(self):
+        self.client.force_login(User.objects.get(first_name='Stranger'))
+        event = self.events.first()
+        participation = {'answer': 'YES', 'event': str(event.pk)}
+        response = self.client.post(reverse('participations:participate'),
+                                    {'participation': [dumps(participation)]})
+        self.assertEqual(403, response.status_code)
+
+    def test_create_one_participation(self):
+        event = self.events.first()
+        with self.assertRaises(Participation.DoesNotExist):
+            Participation.objects.get(participant=self.user, event=event)
+        participation = {'answer': 'YES', 'event': str(event.pk)}
+        response = self.client.post(reverse('participations:participate'),
+                                    {'participation': [dumps(participation)]})
+        self.assertEqual(response.status_code, 200)
+        created_participation = Participation.objects.get(
+            participant=self.user, event=event)
+        self.assertEqual(participation['answer'], created_participation.answer)
+
+    def test_update_one_participation(self):
+        event = self.events.first()
+        existing_participation = Participation.objects.create(
+            participant=self.user, event=event, answer='YES')
+        self.assertEqual(existing_participation.answer, 'YES')
+        participation = {'answer': 'NO', 'event': str(event.pk)}
+        response = self.client.post(reverse('participations:participate'),
+                                    {'participation': [dumps(participation)]})
+        self.assertEqual(response.status_code, 200)
+        existing_participation.refresh_from_db()
+        self.assertEqual(
+            participation['answer'], existing_participation.answer)
+
+    def test_create_two_participation(self):
+        event1 = self.events.first()
+        event2 = self.events.last()
+        with self.assertRaises(Participation.DoesNotExist):
+            Participation.objects.get(participant=self.user)
+        participations = [{'answer': 'YES', 'event': str(event1.pk)},
+                          {'answer': 'NO', 'event': str(event2.pk)}]
+        json_participations = [dumps(part) for part in participations]
+        response = self.client.post(reverse('participations:participate'),
+                                    {'participation': json_participations})
+        self.assertEqual(response.status_code, 200)
+        for part in participations:
+            created_part = Participation.objects.get(
+                participant=self.user, event=part['event'])
+            self.assertEqual(part['answer'], created_part.answer)
+
+    def test_one_create_and_one_update(self):
+        event1 = self.events.first()
+        existing_participation = Participation.objects.create(
+            participant=self.user, event=event1, answer='YES')
+        self.assertEqual(existing_participation.answer, 'YES')
+        event2 = self.events.last()
+        with self.assertRaises(Participation.DoesNotExist):
+            Participation.objects.get(participant=self.user, event=event2)
+        participations = [{'answer': 'MAYBE', 'event': str(event1.pk)},
+                          {'answer': 'NO', 'event': str(event2.pk)}]
+        json_participations = [dumps(part) for part in participations]
+        response = self.client.post(reverse('participations:participate'),
+                                    {'participation': json_participations})
+        self.assertEqual(response.status_code, 200)
+        for part in participations:
+            created_part = Participation.objects.get(
+                participant=self.user, event=part['event'])
+            self.assertEqual(part['answer'], created_part.answer)
--- participations/views.py
+++ participations/views.py
@@ -1,20 +1,44 @@
+from json import loads
+
 from django.contrib.auth.decorators import login_required
-from django.forms import modelform_factory, HiddenInput
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 from accounts.models import User
 from participations.models import Participation
-from plannings.models import Planning
+from plannings.models import Planning, Event
 
 
+@require_POST
 def participate(request):
-    import pdb; pdb.set_trace()
+    if not request.user.is_authenticated:
+        return HttpResponse('Veuillez vous connecter', status=401)  # TODO: changer erreur code? 403?
+    for json_part in request.POST.getlist('participation'):
+        participation = loads(json_part)
+        # TODO: gestion no event or no answer. get_object_or_404? Mais je ne
+        #   veux pas forcement interrompre la requete si seulement un object
+        #   n'est pas conforme...
+        #   Forbidden interrompt la boucle, mais une partie des objects est
+        #   déjà crée... Use block atomic?
+        event = Event.objects.get(pk=participation['event'])
+        planning = event.planning
+        if planning.protected and \
+                request.user.email not in planning.get_guest_emails:
+            return HttpResponseForbidden()
+        Participation.objects.update_or_create(
+            participant=request.user, event=event,
+            defaults={'answer': participation['answer']}
+        )
+    return HttpResponse('Votre participation est enregistrée')  # TODO: trad
 
+
+# TODO:
+#   -gestion de planning inexistant, get_by_ekey_or_404 ou message custom
+#   -utilisation d'un modelForm avec widget RadioSelect
+#   -refactor en plusieurs fonctions
 @login_required
 def view_planning(request, planning_ekey):
-    ParticipationForm = modelform_factory(Participation, fields=('answer', 'event'), widgets={'event': HiddenInput()})
-    # TODO: gestion de planning inexistant.
-    #  get_by_ekey_or_404 ou message custom
     planning = Planning.objects.get_by_ekey(planning_ekey)
     if planning.protected and \
             request.user.email not in planning.get_guest_emails:
@@ -37,8 +61,6 @@ def view_planning(request, planning_ekey):
         # in the same order that the other_participants list.
         # Each item will be the participation if founded, or the default None
         event.other_participations = [None]*len(other_participants)
-        event.user_participation = {'answer_choices': Participation.answer_choices}  # FIXME bricolage?
-        event.user_participation_form = ParticipationForm(initial={'event': event.pk})
         # Participations_summary is a dict {answer: list of participants name},
         # to display a summary of the answers in the planning
         event.participations_summary = {'YES': [], 'MAYBE': [], 'NO': []}
@@ -46,15 +68,15 @@ def view_planning(request, planning_ekey):
             event.participations_summary[part.answer].append(
                 part.participant.first_name)
             if part.participant == request.user:
-                event.user_participation['answer'] = part.answer  # FIXME bricolage?
-                event.user_participation_form = ParticipationForm(instance=part)
+                event.user_participation = part
             elif part.participant in other_participants:
                 p_index = other_participants.index(part.participant)
                 event.other_participations[p_index] = part
 
     context = {'planning': planning,
                'other_participants': other_participants,
-               'events': events}
+               'events': events,
+               'answer_choices': Participation.answer_choices}
     return render(request,
                   'participations/view_planning.html', context)
 
