7fb97425e4d95930e7512def0c6ce816334bee41
SylvainCatillon <sylvain.catillon@gmail.com>
2020-06-10 16:24:24 +0200

    Footer and legal notice

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('legal/', views.legal, name='legal'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """View of the main page."""
     return render(request, 'core/index.html')
+
+
+def legal(request):
+    """View of the legal notice page"""
+    return render(request, "core/legal_notice.html")
